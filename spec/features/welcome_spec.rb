f497c0ef12ec48eab1d688c02ccc5a03b18d35d3
KaraJH <karas.jones@yahoo.com>
2023-03-25 15:58:39 -0600

    test: Sad and happy path test for search

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,11 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Welcome Page" do 
-    it 'has a form to search for activities' do 
+    before(:each) do 
         visit root_path
-        
-        expect(page).to have_content("Activity Finder")
-        expect(page).to have_field(:num_participants)
-        expect(page).to have_button("Find Activities")
-    end 
+    end
+
+    describe "welcome page", :vcr do 
+        it 'has a form to search for activities' do 
+            
+            expect(page).to have_content("Activity Finder")
+            expect(page).to have_field(:num_participants)
+            expect(page).to have_button("Find Activities")
+        end 
+
+        it "takes me to /activity after clicking num of participants" do 
+            fill_in(:num_participants, with: 2)
+            click_button "Find Activities"
+            expect(current_path).to eq("/activity")
+        end
+
+        it "flashes an error message when no participants are entered" do
+            fill_in(:num_participants, with: 0)
+            click_button "Find Activities"
+
+            expect(current_path).to eq(root_path)
+            expect(page).to have_content("Incorrect Input")
+        end
+
+        it "flashes an error message when too many participants are input" do 
+            fill_in(:num_participants, with: 6)
+            click_button "Find Activities"
+
+            expect(current_path).to eq(root_path)
+            expect(page).to have_content("Incorrect Input")
+        end
+    end
 end 
